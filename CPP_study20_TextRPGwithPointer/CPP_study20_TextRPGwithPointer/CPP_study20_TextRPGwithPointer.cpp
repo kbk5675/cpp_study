58505e2943a25d164d4f4b167d7ad1c928152c1b
kbk5675 <kbk980130@gmail.com>
2022-01-29 03:48:49 +0900

    study String Function
    use Pointer 20200129

---
+++ CPP_study20_TextRPGwithPointer/CPP_study20_TextRPGwithPointer/CPP_study20_TextRPGwithPointer.cpp
@@ -0,0 +1,220 @@
+﻿#include <iostream>
+using namespace std;
+
+// TEXT RPG use Pointer
+
+// main
+// - EnterLobby (PlayerInfo)
+// -- CreatePlayer
+// -- EnterGame (MosterInfo)
+// --- CreateMonsters
+// --- EnterBattle
+
+enum PlayerType
+{
+	PT_Knight = 1,
+	PT_Archer = 2,
+	PT_Mage = 3,
+};
+enum MonsterType
+{
+	MT_Slime = 1,
+	MT_Orc = 2,
+	MT_Skeleton = 3,
+};
+
+struct StatInfo
+{
+	int hp = 0;
+	int attack = 0;
+	int denfence = 0;
+};
+
+void EnterLobby();
+void PrintMessage(const char* msg);
+void CreatePlayer(StatInfo* playerInfo);
+void PrintStatInfo(const char* name, const StatInfo& info);
+
+void EnterGame(StatInfo* playerInfo);
+void CreateMonsters(StatInfo monsterInfo[], int count);
+bool EnterBattle(StatInfo* platerInfo, StatInfo* monsterInfo);
+
+
+int main()
+{
+	srand((unsigned)time(nullptr));
+	EnterLobby();
+
+	return 0;
+}
+
+void EnterLobby() {
+	while (true)
+	{
+		PrintMessage("로비에 입장했습니다.");
+
+		//PLAYER!
+		StatInfo playerInfo;
+		CreatePlayer(&playerInfo);
+		PrintStatInfo("Player", playerInfo);
+
+		EnterGame(&playerInfo);
+	}
+}
+
+void PrintMessage(const char* msg)
+{
+	cout << "**************************" << endl;
+	cout << msg << endl;
+	cout << "**************************" << endl;
+}
+
+void CreatePlayer(StatInfo* playerInfo) {
+	
+	bool ready = false;
+	
+	while (ready == false)
+	{
+		PrintMessage("캐릭터 생성창");
+		PrintMessage("[1] 기사  [2] 궁수  [3] 법사");
+		cout << ">> ";
+
+		int input;
+		cin >> input;
+
+		switch (input)
+		{
+		case PT_Knight:
+			playerInfo->hp = 100;
+			(*playerInfo).attack = 15;
+			playerInfo->denfence = 10;
+			ready = true;
+			break;
+		case PT_Archer:
+			playerInfo->hp = 90;
+			(*playerInfo).attack = 20;
+			playerInfo->denfence = 7;
+			ready = true;
+			break;
+		case PT_Mage:
+			playerInfo->hp = 80;
+			(*playerInfo).attack = 25;
+			playerInfo->denfence = 5;
+			ready = true;
+			break;
+
+		default:
+			break;
+		}
+	}
+}
+
+void PrintStatInfo(const char* name, const StatInfo& info)
+{
+	cout << "*********************" << endl;
+	cout << name << " : HP=" << info.hp << " ATT=" << info.attack << " DEF=" << info.denfence << endl;
+	cout << "*********************" << endl;
+}
+
+void EnterGame(StatInfo* playerInfo) {
+	
+	const int MONSTER_COUNT = 2;
+
+	PrintMessage("게임에 입장했습니다.");
+
+	while (true)
+	{
+		StatInfo monsterInfo[MONSTER_COUNT];
+		CreateMonsters(monsterInfo, MONSTER_COUNT);
+
+		for (int i = 0; i < MONSTER_COUNT; i++)
+			PrintStatInfo("Monster", monsterInfo[i]);
+
+		PrintStatInfo("Player", *playerInfo);
+
+		PrintMessage("[1] 전투  [2] 전투 [3] 도망");
+
+		int input;
+		cin >> input;
+
+		if (input == 1 || input == 2) {
+			int index = input - 1;
+			bool victory = EnterBattle(playerInfo, &(monsterInfo[index]) );
+			if (victory == false)
+				break;
+		}
+	}
+}
+
+void CreateMonsters(StatInfo monsterinfo[], int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		int randValue = 1 + rand() % 3;
+
+		switch (randValue)
+		{
+		case MT_Slime:
+			monsterinfo[i].hp = 30;
+			monsterinfo[i].attack = 5;
+			monsterinfo[i].denfence = 1;
+			break;
+
+		case MT_Orc:
+			monsterinfo[i].hp = 40;
+			monsterinfo[i].attack = 8;
+			monsterinfo[i].denfence = 2;
+			break;
+
+		case MT_Skeleton:
+			monsterinfo[i].hp = 50;
+			monsterinfo[i].attack = 10;
+			monsterinfo[i].denfence = 3;
+			break;
+
+		default:
+			break;
+		}
+	}
+}
+
+bool EnterBattle(StatInfo* playerInfo, StatInfo* monsterInfo)
+{
+	while (true)
+	{
+		int damage = 1 + rand() % (playerInfo->attack - monsterInfo->denfence); // 최대 최소 랜덤 데미지.
+		// 데미지 음수 오류 예방.
+		if (damage < 0)
+			damage = 0;
+
+		monsterInfo->hp -= damage;
+		// 몬스터 피통 음수 오류 예방.
+		if (monsterInfo->hp < 0)
+			monsterInfo->hp = 0;
+
+		PrintStatInfo("Monster", *monsterInfo);
+
+		if (monsterInfo->hp == 0)
+		{
+			PrintMessage("몬스터를 처치했습니다.");
+			return true;
+		}
+		//몬스터 안죽었으면 반격.
+		damage = 1 + rand() % (monsterInfo->denfence - playerInfo->attack);
+		if (damage < 0)
+			damage = 0;
+
+		playerInfo->hp -= damage;
+		if (playerInfo->hp < 0)
+			playerInfo->hp = 0;
+
+		PrintStatInfo("Player", *playerInfo);
+
+		if (playerInfo->hp == 0)
+		{
+			PrintMessage("Game Over");
+			return false;
+		}
+	}
+}
+
---
+++ CPP_study21_String01/CPP_study21_String01/CPP_study21_String01.cpp
@@ -0,0 +1,163 @@
+﻿#include <iostream>
+#pragma warning(disable: 4996)
+using namespace std;
+
+
+// 문제1) str이라는 문자열의 길이를 반환
+int StrLen(const char* str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return i;
+}
+
+// 문제2) 문자열 복사 함수
+// [H][e][l][l][o]['\0'][][][][[][][][][][][][][][] src
+// [H][e][l][l][o]['\0'][][][][[][][][][][][][][][] dest
+char* StrCpy(char* dest, char* src)
+{
+	/*NO Pointer*/
+	 
+	//int i = 0;
+
+	//while (dest[i] != '\0') // dest[i]로 두어도 된다. 데이터가 없는 자리는 0이니까.
+	//{
+	//	dest[i] = src[i];
+	//	i++;
+	//}
+	//cout << dest << endl;
+
+	/*Use Pointer*/
+
+	char* ret = dest;
+
+	while (*src != '\0')
+	{
+		//*dest = *src; // 주소데이터 복사
+		//dest++; // 주소증가
+		//src++;  // 주소증가
+		*dest++ = *src++;
+	}
+	*dest = '\0';
+
+	return ret;
+}
+
+// 문제3) 문자열 덧붙히는 함수
+char* StrCat(char* dest, char* src)
+{
+	/*No Pointer*/
+
+	//내가 푼 방법.
+
+	/*int d_len = StrLen(dest);
+	int s_len = StrLen(src);
+	int s = 0;
+	for (int i = d_len; i < d_len+s_len; i++)
+	{
+		dest[i] = src[s];
+		s++;
+	}*/
+	
+
+	//선생님이 푼 방법
+
+	/*int len = StrLen(dest);
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+	dest[len + i] = '\0';
+
+	return dest;
+	*/
+
+	/*Use Pointer*/
+
+	char* ret = dest;
+
+	// 여기서 dest = 5까지 나온겨.
+	while (*dest != '\0')
+		dest++;
+
+	while (*src != '\0')
+		*dest++ = *src++;
+
+	*dest = '\0';
+
+	return ret;
+}
+
+// 문제4) 두 문자열을 비교하는 함수.
+int StrCmp(char* a, char* b)
+{
+	int i = 0;
+	while (a[i] != '\0' || b[i] != '\0')
+	{
+		if (a[i] > b[i])
+			return 1;
+		if (a[i] < b[i])
+			return -1;
+		i++;
+	}
+
+	return 0;
+}
+
+// 문제5) 문자열을 뒤집는 함수.
+void ReversStr(char* str)
+{
+	int len = StrLen(str);
+
+	for (int i = 0; i < len/2; i++)
+	{
+		int temp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = temp;
+	}
+	cout << str << endl;
+}
+
+int main()
+{
+	const int BUF_SIZE = 100;
+
+	char a[BUF_SIZE] = "Hello";
+	char b[BUF_SIZE] = "World";
+
+	if (a == b)
+	{
+		cout << "같다." << endl;
+	}
+	else
+		cout << "다르다." << endl;
+	// 다르다가 나오는 이유. 배열의 데이터는 같으나 배열의 주소는 다르다. 데이터가 같은지 보기위해선? 142번째줄
+
+	int len = sizeof(a);
+	cout << len << endl;
+
+
+	len = StrLen(a);
+	cout << len << endl;
+	 
+	cout << StrCpy(b, a) << endl;
+	cout << StrCat(a, b) << endl;
+
+	char a2[BUF_SIZE] = "a";
+	char b2[BUF_SIZE] = "a";
+
+	int value = strcmp(a2, b2);
+	cout << value << endl;
+
+	StrCmp(a2, b2);
+	ReversStr(a);
+
+	return 0;
+}
