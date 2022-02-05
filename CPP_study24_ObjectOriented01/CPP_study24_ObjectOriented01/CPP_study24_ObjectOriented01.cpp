c7f87b0c8453d320048eadb1ad81f8224a11fccf
kbk5675 <kbk980130@gmail.com>
2022-02-05 15:54:33 +0900

    Explain OOP, Inheritance
    C++ is Object Oriented Programming

---
+++ CPP_study24_ObjectOriented01/CPP_study24_ObjectOriented01/CPP_study24_ObjectOriented01.cpp
@@ -0,0 +1,150 @@
+﻿#include <iostream>
+using namespace std;
+
+// 주제 : 객체지향 프로그래밍 & 생성자와 소멸자
+
+// 절차(procedural)지향 프로그래밍
+// - procedure = 함수
+
+// 절차지향은 데이터를 중심으로 함수를 구현합니다. 이에 반해 객체지향은 기능을 중심으로 메서드를 구현
+
+// 데이터 + 가공 = 프로그램,게임
+
+// Knight를 설계해보자
+// - 속성(데이터) : hp, attack, y, x
+// - 기능(동작) : Move, Attack, Die;
+
+// [생성자(Constructor)와 소멸자(Destructor)]
+// 클래스에 '소속'된 함수들은 멤버 함수라고 함
+// 이 중에서 굉장히 특별한 함수 2종이 있는데, 바로 [시작]과 [끝]을 알리는 함수들
+// - 시작(탄생) -> 생성자 (여러개 존재 가능)
+// - 끝(소멸) -> 소멸자( 오직 1개만)
+
+// [암시적(Implicit) 생성자]
+// 생성자를 명시적으로 만들지 않으면,
+// 아무 인자도 받지 않는 [기본 생성자]가 컴파일러에 의해 자동으로 만들어진다.
+// -> 그러나 우리가 명시적(Explicit)으로 아무 생성자 하나 만들면,
+// 자동으로 만들어지던 [기본 생성자]는 더 이상 만들어지지 않음!
+
+
+class Knight
+{
+public:
+	// 기본 생성자(인자가 없음)
+	Knight()
+	{
+		cout << "Knight() 기본 생성자 호출" << endl;
+	}
+
+	// 복사 생성자
+	// 일반적으로 똑같은 데이터를 지닌 객체가 생성되길 기대한다.
+	Knight(const Knight& Knight)
+	{
+		cout << "Knight(&Knight) 복사 생성자 호출" << endl;
+		_hp = Knight._hp;
+		attack = Knight.attack;
+		posY = Knight.posY;
+		posX = Knight.posX;
+	}
+
+	// int 인수를 받는 기타생성자
+	// 인자를 1개만 받는 생성자는 [타입 변환 생성자]라 부르기도함
+	// explicit -> 명시적인 용도로만 사용할 것!
+	explicit Knight(int hp)
+	{
+		cout << "Knight(int) 기타생성자 호출" << endl;
+		_hp = hp;
+		attack = 0;
+		posY = 0;
+		posX = 0;
+
+	}
+
+	// 기타 생성자
+	Knight(int hp, int attack, int posX, int posY)
+	{
+		_hp = _hp;
+		attack = attack;
+		posY = posY;
+		posX = posX;
+	}
+
+	// 소멸자 Knight()가 소멸 될 때 실행됨.
+	~Knight()
+	{
+		cout << "Knight() 기본 생성자 소멸" << endl;
+	}
+
+	// 멤버함수 선언
+	void Move(int y, int x);
+	void Attack();
+	void Die()
+	{
+		_hp = 0;
+		cout << "Die" << endl;
+	}
+
+public:
+	// 멤버변수 
+	int _hp; // or m_hp, mHp 멤버변수인것을 표시하는게 좋음.
+	int attack;
+	int posY;
+	int posX;
+};
+
+void Knight::Move(int y, int x) {
+
+	posY = y;
+	posX = x;
+	cout << "Move" << endl;
+}
+
+void Knight::Attack()
+{
+	cout << "Attack : " << attack << endl;
+}
+
+void HelloKnight(Knight k)
+{
+	cout << "Hello Knight" << endl;
+}
+
+// instantiate 객체를 만든다.
+
+int main()
+{
+	Knight(100, 10, 1, 1); // 기타생성자 이용!
+
+	Knight k1;
+	k1._hp = 100;
+	k1.attack = 10;
+	k1.posX = 0;
+	k1.posY = 0;
+
+	Knight k2(k1); // 복사 생성자 이용!
+
+	Knight k3 = k1; // 복사 생성자 이용!
+
+
+	// 기본 생성자 이용!
+	Knight k4;
+	k4 = k1; 
+
+	// k1과 k2는 메모리주소가 다르다 서로 16byte
+	k1.Move(2, 2);
+	k1.Attack();
+	k1.Die();
+	 
+
+	// 암시적 형번환 -> 컴파일러가 알아서 바꿔치기
+	int num = 1;
+
+	float f = (float)num; // 명시적 < 우리가 코드로 num을 float 바구니에 넣으라고 주문하고 있음
+	double d = num; // 암시적 << 별말 안했는데 컴파일러가 알아서 처리하고 있음
+	
+
+	Knight k5;
+
+
+	return 0;
+}
---
+++ CPP_study25_Inheritance/CPP_study25_Inheritance/CPP_study25_Inheritance.cpp
@@ -0,0 +1,146 @@
+﻿#include <iostream>
+using namespace std;
+
+// 오늘의 주제 : 상속성
+
+// 객체지향 (OOP Object Oriented Programming)
+// - 상속성 =  부모 -> 자식
+// - 은닉성 
+// - 다형성
+
+// class는 일종의 설계도!
+
+// 생성자(N)/소멸자(1)
+
+// 생성자는 탄생을 기념해서 호출되는 함수?
+// - Knight를 생성하면 -> Player의 생성자 호출? Knight의 생성자를 호출??
+//   -> 둘다 호출한다 ^^. (생성자는 상속에서 제외)
+
+//GameObject
+// - Creature
+// - Player, Monster, NPC, Pet
+// - Projectile
+// - Arrow, Fireball
+// - Env
+
+//Item
+// - Weapon
+// - Sword, Bow
+// - Armor
+// - Helmet, Boots
+// - consumable
+// - Potion, Scroll
+
+
+//Player는 부모클래스
+class Player
+{
+public:
+	Player()
+	{
+		_hp = 0;
+		_attack = 0;
+		_defence = 0;
+		cout << "Player() 기본 생성자 호출" << endl;
+	}
+
+	Player(int hp)
+	{
+		_hp = hp;
+		_attack = 0;
+		_defence = 0;
+		cout << "Player(int hp) 생성자 호출" << endl;
+	}
+
+	~Player()
+	{
+		cout << "~Player() 소멸자 호출" << endl;
+	}
+
+	void Move() { cout << "Player Move 호출" << endl; }
+	void Attack() { cout << "Player Attack 호출" << endl; }
+	void Die() { cout << "Player Die 호출" << endl; }
+
+public:
+	int _hp;
+	int _attack;
+	int _defence;
+};
+// Knight뿐만 아니라 많은 직업이 있는데 하나하나 클래스 만들기 부담스러움. ==> 객체지향의 [상속]을 사용하자!
+// 
+// 메모리 구성
+// [ [ Player ] ]
+// [    Knight  ]
+// 
+// 
+// Knight는 자식클래스
+class Knight : public Player
+{
+public:
+	Knight()
+	{
+		/* 생성자 호출순서
+		-선처리 영역
+		--..
+		---...
+		----.... 처리 마지막쯤  Player() 생성자 호출
+		*/
+		_stamina = 100;
+		cout << "Knight() 기본 생성자 호출" << endl;
+	}
+
+	Knight(int stamina) : Player(100)
+	{
+		/* 생성자 호출순서
+				-선처리 영역
+				--..
+				---...
+				----.... 처리 마지막쯤  Player(int hp) 생성자 호출
+				*/
+		_stamina = stamina;
+		cout << "Knight(_stamina)  생성자 호출" << endl;
+	}
+
+	~Knight()
+	{
+		cout << "~Knight() 소멸자 호출" << endl;
+	}
+	/* 소멸자 호출순서
+		-후처리 영역
+		- ~Player() 소멸자 호출
+		*/
+
+
+
+	// 재정의 : 부모님의 유산을 거부하고 새로은 이름으로 새로운 기능 만든?
+	void Move() { cout << "Knight Move 호출" << endl; }
+public:
+	int _stamina;
+};
+
+// Mage는 자식클래스
+class Mage : public Player
+{
+public:
+
+public:
+	int _mp;
+};
+
+
+int main()
+{
+	Knight k(100);
+
+	k._hp = 100;
+	k._attack = 10;
+	k._defence = 5;
+	k._stamina = 50;
+
+	k.Attack();
+	k.Die();
+	k.Move();
+
+
+	return 0;
+}
