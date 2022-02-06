5b80de867308a261bb24f1ced29675f136503977
kbk5675 <kbk980130@gmail.com>
2022-02-06 23:53:33 +0900

    Polymorphism
    study Polymorphism

---
+++ CPP_stduy27_Polymorphism/CPP_stduy27_Polymorphism/CPP_stduy27_Polymorphism.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+using namespace std;
+
+// 오늘의 주제 : 다형성
+
+// 다형성(Polymorphism = poly + morph) = 겉은 똑같은데, 기능이 다르게 동작한다.
+// - 오버로딩(Overloading) = 함수 중복 정의 = 함수 이름의 재사용
+// - 오버라이딩(Overriding) = 재정의 = 부모 클래스의 함수를 자식 클래스에서 재정의
+
+// 바인딩(Binding) = 묶는다.
+// - 정적 바인딩(Static Binding) : 컴파일 시점에 결정
+// - 동적 바인딩(Static Binding) : 실행 시점에 결정
+
+// 일반 함수는 정적 바인딩 사용.
+// 동적 바인딩을 원한다면? -> 가상 함수 (Virtual function)
+// 그런데 실제 객체가 어떤 타입인지 어떻게 알고 알아서 가상함수를 호출해준걸까?
+// -> 가상 함수 테이블 생성! (vftable)
+
+// .vftable [] 4바이트(32) 8바이트(64)
+
+// [VMove] [ ]
+
+// 순수 가상 함수 : 구현은 없고 '인터페이스'만 전달하는 용도로 사용하고 싶을 때 사용.
+
+class Player
+{
+public:
+
+	Player()
+	{
+		_hp = 100;
+	}
+
+	void Move() { cout << "Move Player !" << endl; }
+
+	void Move(int a) { cout << "Move Player (int) !" << endl; }
+
+	virtual void VMove() { cout << "Move Player (virtual) !" << endl; }
+	virtual void VDie() { cout << "Die Player (virtual) !" << endl; }
+
+	// 순수 가상 함수 (= 0;)
+	virtual void VAttack() = 0;
+
+public:
+	int _hp;
+};
+
+class Knight : public Player
+{
+public:
+
+	Knight()
+	{
+		_stamina = 100;
+	}
+
+	void Move() { cout << "Move Knight !" << endl; }
+
+	//가상함수는 재정의를 하더라도 가상함수다!
+	virtual void VMove() { cout << "Move Knight (virtual) !" << endl; }
+	virtual void VDie() { cout << "Die Knight (virtual) !" << endl; }
+
+	virtual void VAttack() { cout << "VAttack knight" << endl; }
+
+
+public:
+	int _stamina;
+};
+
+
+
+
+// [ [ player ] ]
+// [   knight   ]
+void MovePlayer(Player* player)
+{
+	player->VMove();
+	player->VDie();
+}
+
+void MoveKnight(Knight* knight)
+{
+	knight->Move();
+}
+
+int main()
+{
+	/*Player p;
+	p.Move();
+	p.Move(10);*/
+
+	Player p; // 순수가상함수를 포함하면 해당 클래스를 독단적으로 사용할 수 없다!
+	//MovePlayer(&p); // 플레이어는 플레이어다? Yes
+	//MoveKnight(&p); // 플레이어는 기사다? No --> 알 수 있는것 : 부모클래스에선 자식클래스로 넘어갈 수 없다.
+
+	Knight k;
+	//MoveKnight(&k); // 기사는 기사다? Yes
+	MovePlayer(&k); // 기사는 플레이어다? Yes
+	return 0;
+}
---
+++ CPP_study26_HiddenProperty/CPP_study26_HiddenProperty/CPP_study26_HiddenProperty.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+using namespace std;
+
+// 오늘의 주제 : 은닉성
+
+// 은닉성(Data Hiding) = 캡슐화(Encapsulation)
+// 몰라도 되는 것은 깔끔하게 숨기겠다!
+// 숨기는 이유?
+// - 정말 위험하고 건드리면 안되는 경우
+// - 다른 경로로 접근하길 원하는 경우
+
+// 자동창
+// -핸들
+// -페달
+// -엔진
+// -문
+// -각종 전기선
+
+// 일반 구매자 입장에서 사용하는 것?
+// -핸들/페달/문
+// 몰라도되는부분
+// -엔진/각종 전기선
+
+// public(공개적) protected(보호받는) private(개인의)
+// - public : 누구나 쓰세요.
+// - protected : 나의 자손들에게만 허락(상속클래스만 사용가능)
+// - private : 나만 사용할꺼! << class Car 내부에서만 사용
+
+// 상속 접근 지정자 : 다음 세대한테 부모님의 유산을 어떻게 물려줄지?
+// 부모님한테 물려받은 유산을 꼭 나의 자손들한테 똑같이 물려줘야 하진 않음
+// - public : 공개적 상속 -> 부모님의 유산 설계 그대로(public -> public, protected -> protected)
+// - protected : 보호받는 상속 -> 내 자손들한테만 물려줄거야(public -> protected, protected -> protected)
+// - private : 개인적인 상속 -> 나까지만 잘 쓰고 자손들한텐 안물려줄거야(public -> private, protected -> private)
+
+class Car
+{
+public: // 멤버 접근 지정자
+	void MoveHandle() {}
+	void PushRedal() {}
+	void OpenDoor() {}
+
+	void TurnKey()
+	{
+		RunEngine();
+	}
+
+//private: // 상속 클래스도 사용불가.
+protected:
+	void DisassembleCar() {} // 차를 분해한다
+	void RunEngine() {} // 엔진을 구동시킨다
+	void ConnectCircuit() {} // 전기선 연결
+
+public:
+	// 핸들
+	// 페달
+	// 엔진
+	// 문
+	// 각종 전기선
+};
+
+class SuperCar : private Car // 상속 접근 지정자
+{
+public:
+	void PushRemoteController()
+	{
+		RunEngine();
+	}
+};
+
+class TestSuperCar : SuperCar 
+{
+public:
+	void Test()
+	{
+		DisassembleCar(); // 사용 못함 SuperCar가 Car를 private으로 상속 받았기 때문이다.
+	}
+};
+
+
+// '캡슐화'
+// 연관된 데이터와 함수를 논리적으로 묶어놓은 것
+class Berserker
+{
+public:
+
+	int GetHp() { return _hp; }
+
+	// 사양 : 체력이 50 이하로 떨어지면 버서커 모드 발동 (강해짐)
+	void SetHp(int hp)
+	{
+		_hp = hp;
+		if (_hp < 50)
+			SetBerserkerMode();
+	}
+
+private:
+	void SetBerserkerMode()
+	{
+		cout << "Berserker Mode On" << endl;
+	}
+
+private:
+	int _hp = 100;
+};
+
+int main()
+{
+	Car car;
+	car.OpenDoor();
+	// car.RunEngine(); //엑세스 불가에러 -> private이기 때문.
+
+	Berserker b;
+	// b._hp = 20; // _hp를 public으로 설정하게 두어서 버서커모드 똑바로 작동 안함. 설계 오류! -> 캡슐화 필요
+	b.SetHp(30);
+
+	return 0;
+}
