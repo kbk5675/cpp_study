ef254d4ae0bdae6f43662b9ee0e9e633b6f9afa4
Bogoomi <56444899+kbk5675@users.noreply.github.com>
2022-10-07 14:46:32 +0900

    Add files via upload

---
+++ Algorithm/Tree_Original.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+using NodeRef = shared_ptr<struct Node>;
+
+struct Node
+{
+	Node() { }
+	Node(const string& data) : data(data) { }
+
+	string data;
+	vector<NodeRef> children;
+};
+
+NodeRef CreateTree()
+{
+	NodeRef root = make_shared<Node>("R1 개발실");
+	{
+		NodeRef node = make_shared<Node>("디자인팀");
+		root->children.push_back(node);
+		{
+			NodeRef leaf = make_shared<Node>("전투");
+			node->children.push_back(leaf);
+		}
+		{
+			NodeRef leaf = make_shared<Node>("스토리");
+			node->children.push_back(leaf);
+		}
+		{
+			NodeRef leaf = make_shared<Node>("경제");
+			node->children.push_back(leaf);
+		}
+	}
+	{
+		NodeRef node = make_shared<Node>("프로그래밍팀");
+		root->children.push_back(node);
+		{
+			NodeRef leaf = make_shared<Node>("서버");
+			node->children.push_back(leaf);
+		}
+		{
+			NodeRef leaf = make_shared<Node>("클라");
+			node->children.push_back(leaf);
+		}
+		{
+			NodeRef leaf = make_shared<Node>("엔진");
+			node->children.push_back(leaf);
+		}
+	}
+	{
+		NodeRef node = make_shared<Node>("아트팀");
+		root->children.push_back(node);
+		{
+			NodeRef leaf = make_shared<Node>("배경");
+			node->children.push_back(leaf);
+		}
+		{
+			NodeRef leaf = make_shared<Node>("캐릭터");
+			node->children.push_back(leaf);
+			{
+				NodeRef leaf2 = make_shared<Node>("더미");
+				leaf->children.push_back(leaf2);
+			}
+		}
+	}
+
+	return root;
+}
+
+void PrintTree(NodeRef root, int depth)
+{
+	for (int i = 0; i < depth; i++)
+	{
+		cout << '-';
+	}
+
+	cout << root->data << endl;
+
+	for (NodeRef& child : root->children)
+		PrintTree(child, depth + 1);
+}
+
+// 깊이(depth) : 루트에서 어떤 노드에 도달하기 위해 거쳐야 하는 간선의 수
+// 높이(height): 가장 깊숙히 있는 노드의 깊이 (max(depth))
+int GetHeight(NodeRef root)
+{
+	int height = 1;
+
+	for (NodeRef& child : root->children)
+	{
+		height = max(height, GetHeight(child) + 1); 
+	}
+
+	return height;
+}
+ 
+int main()
+{
+	NodeRef root = CreateTree();
+	PrintTree(root, 0);
+	 
+	int height = GetHeight(root);
+
+	cout << "Tree Height : " << height << endl;
+
+	return 0;
+}
